ad72e8653e301f3cb7a31bb5e6a0f4cb13d47760
Resul Ablazizov <resul@example.com>
2026-06-26 01:05:14 +0600

    Был создан один компанент Главное страницы без написание определенной разметки,  сегодня работа только над разметкой и стилямь навигационой панели

---
+++ vite-project/src/component/Glavnoe.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+
+function Glavnoe (){
+    return(
+      <div>
+        
+        <div className='glavnoe'>
+          <h1>Список дел</h1> 
+           
+
+        </div>
+      </div>
+    )
+}
+ 
+export default Glavnoe; 
